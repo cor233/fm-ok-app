64610c887e02f66b2e83f47cffb410f08266a6fa
FongMi <b8892778@gmail.com>
2025-04-05 01:17:32 +0800

    Update to 3.4.9

---
+++ RELEASE.md
@@ -0,0 +1,9 @@
+# 電視版 3.4.9
+
+* [Vod]
+* Fix danmaku load
+
+# 手機版 3.4.9
+
+* [Vod]
+* Fix danmaku load
---
+++ apk/dev.bat
@@ -0,0 +1,8 @@
+move .\leanbackArm64_v8a\release\*.apk .\dev
+move .\leanbackArmeabi_v7a\release\*.apk .\dev
+move .\mobileArm64_v8a\release\*.apk .\dev
+move .\mobileArmeabi_v7a\release\*.apk .\dev
+rmdir /s /q .\leanbackArm64_v8a
+rmdir /s /q .\leanbackArmeabi_v7a
+rmdir /s /q .\mobileArm64_v8a
+rmdir /s /q .\mobileArmeabi_v7a
